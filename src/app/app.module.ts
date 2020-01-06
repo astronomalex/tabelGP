c957e38d9eb49972c162c25040a5bef1269ed959
greenp84 <greenp84@gmail.com>
2020-01-06 14:37:35 +0300

    create new folders, restructure files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SmenListComponent } from './smen-list/smen-list.component';
-import { WorkerListComponent } from './worker-list/worker-list.component';
+import { SmenListComponent } from './tabel/smen-list/smen-list.component';
+import { WorkerListComponent } from './workers/worker-list/worker-list.component';
 
 @NgModule({
   declarations: [
