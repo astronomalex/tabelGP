c8cf8a28503dd6f47be0848b7d1c8e70f2259778
greenp84 <greenp84@gmail.com>
2020-01-06 13:31:41 +0300

    Created files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SmenListComponent } from './smen-list/smen-list.component';
+import { WorkerListComponent } from './worker-list/worker-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmenListComponent,
+    WorkerListComponent
   ],
   imports: [
     BrowserModule,
