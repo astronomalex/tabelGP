9b8dd2f6d2b9bc11d29b2404b0f634578e0ac626
astronomalex <52239193+astronomalex@users.noreply.github.com>
2020-02-19 12:03:54 +0300

    Added the authentication
    With error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,28 +5,31 @@ import {SmenListStartComponent} from './tabel/smen-list/smen-list-start/smen-lis
 import {SmenaDetailComponent} from './tabel/smena-detail/smena-detail.component';
 import {TabelComponent} from './tabel/tabel.component';
 import {SmenListResolverService} from './tabel/smen-list-resolver.service';
-import {WorkerListComponent} from './workers/worker-list/worker-list.component';
 import {WorkerListStartComponent} from './workers/worker-list/worker-list-start/worker-list-start.component';
 import {WorkersComponent} from './workers/workers.component';
 import {WorkerDetailComponent} from './workers/worker-detail/worker-detail.component';
 import {WorkerDataEditComponent} from './workers/worker-data-edit/worker-data-edit.component';
+import {AuthComponent} from './auth/auth.component';
+import {AuthGuard} from './auth/auth.guard';
+import {WorkerListResolverService} from './workers/worker-list-resolver.service';
 
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/smen-list', pathMatch: 'full'},
-  {path: 'smen-list', component: TabelComponent, children: [
+  {path: 'smen-list', component: TabelComponent, canActivate: [AuthGuard], children: [
       {path: '', component: SmenListStartComponent},
       {path: 'new', component: SmenEditComponent},
       {path: ':id', component: SmenaDetailComponent, resolve: [SmenListResolverService]},
       {path: ':id/edit', component: SmenEditComponent, resolve: [SmenListResolverService]}
     ] },
-  {path: 'worker-list', component: WorkersComponent, children: [
+  {path: 'worker-list', component: WorkersComponent, canActivate: [AuthGuard], children: [
       {path: '', component: WorkerListStartComponent},
       {path: 'new', component: WorkerDataEditComponent},
-      {path: ':id', component: WorkerDetailComponent},
-      {path: ':id/edit', component: WorkerDataEditComponent}
-    ]}
+      {path: ':id', component: WorkerDetailComponent, resolve: [WorkerListResolverService]},
+      {path: ':id/edit', component: WorkerDataEditComponent,  resolve: [WorkerListResolverService]}
+    ]},
+  {path: 'auth', component: AuthComponent}
   ]
 ;
 
